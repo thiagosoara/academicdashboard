b7e4aac6669a259282829c4c80c2cf35659a630f
Thiago Araújo <thiagosoara@gmail.com>
2020-09-11 21:49:38 -0300

    adicionando as páginas de disciplina e disciplina details

--- js/functions.js
+++ js/functions.js
@@ -15,25 +15,29 @@ function getUrlParams(urlOrQueryString) {
     return {};
   }
   
-  /**
-   * Helper function for `getUrlParams()`
-   * Builds the querystring parameter to value object map.
-   *
-   * @param queryString {string} - The full querystring, without the leading '?'.
-   */
-  function _mapUrlParams(queryString) {
-    return queryString    
-      .split('&') 
-      .map(function(keyValueString) { return keyValueString.split('=') })
-      .reduce(function(urlParams, [key, value]) {
-        if (Number.isInteger(parseInt(value)) && parseInt(value) == value) {
-          urlParams[key] = parseInt(value);
-        } else {
-          urlParams[key] = decodeURI(value);
-        }
-        return urlParams;
-      }, {});
-  }
+  function getUrlParameters(parameter, staticURL, decode){
+    /*
+     Function: getUrlParameters
+     Descrição: Obtem o valor dos parâmetros da URL atual ou URL estática
+     Author: Tirumal
+     URL: www.code-tricks.com
+    */
+    var currLocation = (staticURL.length)? staticURL : window.location.search,
+        parArr = currLocation.split("?")[1].split("&"),
+        returnBool = true;
+ 
+    for(var i = 0; i < parArr.length; i++){
+         parr = parArr[i].split("=");
+         if(parr[0] == parameter){
+             return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+             returnBool = true;
+         }else{
+             returnBool = false;            
+         }
+    }
+ 
+    if(!returnBool) return false;  
+ }
 
   // home.html function
   function getAllCourses(data){
@@ -53,3 +57,61 @@ function getUrlParams(urlOrQueryString) {
       });
       return tamanho;
   }
+
+  function getCourseByName(data,course){
+    array = data["courses"];
+    var c = new Object();
+    array.forEach(element => {
+      if (element.course_name == course){
+        c = element;
+      }
+    });
+    return c;
+  }
+
+  function getSubjectByNameInCourse(courseObject, subject){
+    array = courseObject["subjects"];
+    var s = new Object();
+    array.forEach(element => {
+      if (element.subject_name == subject){
+        s = element;
+      }
+    });
+    return s;
+  }
+
+  function getSubjectsByCourse(data,course){
+    array = data["courses"];
+    subjects = [];
+    array.forEach(element => {
+      if (element.course_name == course){
+        subjects = element.subjects;
+      }  
+    });
+    subs = [];
+    subjects.forEach(element => {
+      var s = new Object();
+      s["name"] = element.subject_name;
+      s["value"] = element.users.students.length;
+      subs.push(s);
+    });
+    return subs;
+  }
+
+  function getUserAccessBySubject(data, course, subject){
+    //array = data["courses"].course_name.["subjects"].subject_name
+    courseObject = getCourseByName(data,course);
+    subjectObject = getSubjectByNameInCourse(courseObject, subject);
+    users = subjectObject["users"].students;
+    dados = [];
+    users.forEach(element => {
+      var u = new Object();
+      u["category"] = element.firstname;
+      u["value"] = element.n_access.length;
+      dados.push(u);
+    });
+    return dados;
+  }
+
+
+
